c39f31d951d1aeb9e289c5f01b45b52b6ba70b57
s20796 <101294179+s20796@users.noreply.github.com>
2023-04-29 14:03:52 +0200

    Add files via upload

--- app_diagnoza.py
+++ app_diagnoza.py
@@ -30,18 +30,19 @@ def main():
 
 	with right:
 		objawy_slider = st.slider("Objawy", value=1, min_value=1, max_value=5)
-		choroby_wsp_slider = st.slider("Wiek", min_value=11, max_value=77)
+		wiek_slider = st.slider("Wiek", min_value=11, max_value=77)
+		choroby_wsp_slider = st.slider("Choroby Współistniejące", min_value=0, max_value=5)
 		wzrost_slider = st.slider("Wzrost", min_value=159, max_value=200)
 		leki_slider = st.slider("Leki", min_value=1, max_value=4)
 
-	data = [[objawy_slider, choroby_wsp_slider, wzrost_slider, leki_slider]]
-	survival = model.predict(data)
+	data = [[objawy_slider, wiek_slider, choroby_wsp_slider, wzrost_slider, leki_slider]]
+	zdrowie = model.predict(data)
 	s_confidence = model.predict_proba(data)
 
 	with prediction:
 		st.subheader("Czy taka osoba jest chora?")
-		st.subheader(("Tak" if survival[0] == 1 else "Nie"))
-		st.write("Pewność predykcji {0:.2f} %".format(s_confidence[0][survival][0] * 100))
+		st.subheader(("Tak" if zdrowie[0] == 1 else "Nie"))
+		st.write("Pewność predykcji {0:.2f} %".format(s_confidence[0][zdrowie][0] * 100))
 
 if __name__ == "__main__":
     main()
